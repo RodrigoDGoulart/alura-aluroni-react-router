94ac2849462647f02fc9ae2fdaa7ef1998370ed4
Rodrigo Diniz Goulart <rodrigo.goulart@img.com.br>
2022-12-26 13:10:09 -0300

    Página NotFound para Pratos

--- src/pages/Cardapio/Itens/Item/item.tsx
+++ src/pages/Cardapio/Itens/Item/item.tsx
@@ -1,10 +1,13 @@
 import styles from './Item.module.scss';
 import { Prato } from 'types/Prato';
 import TagsPrato from 'components/TagsPrato/tagsPrato';
+import { useNavigate } from 'react-router-dom';
 
 export default function Item(props: Prato) {
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.item}>
+    <div className={styles.item} onClick={() => navigate(`/prato/${props.id}`)}>
       <div className={styles.item__imagem}>
         <img src={props.photo} alt={props.title} />
       </div>
